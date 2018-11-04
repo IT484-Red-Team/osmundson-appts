4ceec52cf224f5b6274fe8989eb6f250132a0868
Dylan <dylan.jech@mnsu.edu>
2018-11-04 05:02:28 +0000

    Successgul login redirects to Apply Page

--- app/controllers/rent_apps_controller.rb
+++ app/controllers/rent_apps_controller.rb
@@ -1,6 +1,10 @@
 class RentAppsController < ApplicationController
+    before_action :store_user_location!, if: :storable_location?
+    # The callback which stores the current location must be added before you authenticate the user 
+    # as `authenticate_user!` (or whatever your resource is) will halt the filter chain and redirect 
+    # before the location can be stored.
     before_action :authenticate_user!
-
+    
     def new
         # renders 'new' template
     end
@@ -10,4 +14,24 @@ class RentAppsController < ApplicationController
         flash[:notice] = "Application successfully submitted."
         redirect_to root_path
     end
+
+    private
+    # Its important that the location is NOT stored if:
+    # - The request method is not GET (non idempotent)
+    # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an 
+    #    infinite redirect loop.
+    # - The request is an Ajax request as this can lead to very unexpected behaviour.
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+    end
+
+    def store_user_location!
+        # :user is the scope we are authenticating
+        store_location_for(:user, request.fullpath)
+    end
+    
+    def after_sign_in_path_for(resource_or_scope)
+        stored_location_for(resource_or_scope) || super
+    end
+
 end
