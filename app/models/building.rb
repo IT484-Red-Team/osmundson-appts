4e525ebea80fe4b9c9446e4d051d08c2129aaf4b
Dylan <dylan.jech@mnsu.edu>
2018-11-08 23:15:39 +0000

    Added Buildings to apply form

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,4 +1,12 @@
 class Building < ActiveRecord::Base
     has_many :apartment
     belongs_to :owner, optional: true
+    
+    def self.all_street_addrs
+        street_addrs = []
+        Building.find_each do |b|
+           street_addrs.push(b.street_addr) 
+        end
+        return street_addrs
+    end
 end
