5b1464fa3bb4b37b277336cb7d1f4ad6ab374771
Sahan Modaraarachchi <sahan.modara-arachchi@mnsu.edu>
2018-11-09 03:28:06 +0000

    re-configured the redirection after sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,25 @@ class User < ActiveRecord::Base
     devise :database_authenticatable, :registerable,  
          :recoverable, :rememberable, :trackable, :validatable,:omniauthable,  omniauth_providers: [:google_oauth2]
     def self.find_for_open_id(access_token, signed_in_resource=nil)
-        data = access_token.extra.raw_info
-        if user = User.where(:email => data.email).first
-          user
-        else # Create a user with a stub password. 
-          user = User.new(:email => data.email, :password => Devise.friendly_token[0,20])
-          user.save! 
+        # data = access_token.extra.raw_info
+        # if user = User.where(:email => data.email).first
+        #   user
+        # else # Create a user with a stub password. 
+        #   user = User.new(:email => data.email, :password => Devise.friendly_token[0,20])
+        #   user.save! 
+        # end
+        data = access_token.info
+        user = User.where(email: data['email']).first
+    
+        # Uncomment the section below if you want users to be created if they don't exist
+        unless user
+            user = User.create(
+                email: data['email'],
+                password: Devise.friendly_token[0,20]
+            )
         end
+        user
     end
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_063208) do
+ActiveRecord::Schema.define(version: 2018_11_08_224233) do
 
   create_table "apartments", force: :cascade do |t|
     t.string "number"
