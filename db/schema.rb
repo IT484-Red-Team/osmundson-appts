ad541d6a88e336ec6df2f767880b59f1131a5a62
Dylan <dylan.jech@mnsu.edu>
2018-11-10 02:50:36 +0000

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_025150) do
+ActiveRecord::Schema.define(version: 2018_11_08_224233) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "rent_app_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_11_08_025150) do
 
   create_table "buildings", force: :cascade do |t|
     t.string "number"
+    t.string "name"
     t.string "street_addr"
     t.integer "owner_id"
     t.index ["owner_id"], name: "index_buildings_on_owner_id"
