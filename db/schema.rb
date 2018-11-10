d6af1f5a23cbeb9d1d1833a15c60255468ca7ec2
Alec Pierce <alexander.pierce@mnsu.edu>
2018-11-10 02:31:03 +0000

    Added a new test for apartments

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_11_08_224233) do
 
   create_table "buildings", force: :cascade do |t|
     t.string "number"
+    t.string "name"
     t.string "street_addr"
     t.integer "owner_id"
     t.index ["owner_id"], name: "index_buildings_on_owner_id"
