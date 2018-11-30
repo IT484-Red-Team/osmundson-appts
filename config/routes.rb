b5b21aea67bcc0b2d78b3c232adee102ddf16d2a
Chase Koch <chase.koch@mnsu.edu>
2018-11-30 20:31:08 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'apartments/index'
-  resource :apartments
-  resource :rent_app
-=======
   resources :apartments
   resources :rent_apps
   resources :buildings
->>>>>>> b7667b35625e12de6d94e6b3342bd749c5db3cab
   get 'apartments/new'
   post 'apartments/create'
   get 'rent_app/new'
